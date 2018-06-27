89c18643196e5288e35f67f1ec2c0c30e5f1bb8c
Morris Jobke <hey@morrisjobke.de>
2018-06-27 22:31:27 +0200

    Add current state
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

---
+++ 8 Single File Components.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>Hello {{ pilot }}!</p>
+  <cockpit-component />
+</template>
+
+<script>
+import CockpitComponent from './CockpitComponent.vue'
+
+module.exports = {
+  name: 'Millenium Falcon',
+  data: function() {
+    return {
+      pilot: 'Chewbacca'
+    }
+  },
+  components: {
+    CockpitComponent
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 32px;
+  text-align: center;
+}
+</style>
